2c409e673271efde0cb0c56e8601d504df872711
serhii.lekariev <serhii.lekariev@teamdev.com>
2020-05-13 13:49:20 +0300

    Debug the build.

--- install-flutter.ps1
+++ install-flutter.ps1
@@ -2,4 +2,5 @@
 ./flutter/bin/flutter channel master
 ./flutter/bin/flutter upgrade
 ./flutter/bin/flutter config --enable-windows-desktop
+echo "BUILDING FOR WINDOWS"
 ./flutter/bin/flutter build windows
